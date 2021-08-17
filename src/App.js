c90d04b7181c7eca88ce582adf0277b28c9d8401
janchua19 <jan.edward.chua@oocl.com>
2021-08-17 13:34:54 +0800

    [TodoGroup][Jan] do todoGroup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoGroup from './features/todos/components/TodoGroup';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoGroup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/common/constants/constants.js
@@ -0,0 +1,27 @@
+export const initialTodoList = [
+    {
+        id: "1",
+        text: "SAMPLE 1",
+        done: true
+    },
+    {
+        id: "2",
+        text: "SAMPLE 2",
+        done: false
+    },
+    {
+        id: "3",
+        text: "SAMPLE 3",
+        done: false
+    },
+    {
+        id: "4",
+        text: "SAMPLE 4",
+        done: false
+    },
+    {
+        id: "5",
+        text: "SAMPLE 5",
+        done: false
+    }
+];
